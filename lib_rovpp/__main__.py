64dc25239f904d8c37455f08f7e080bd4b26f295
Justin Furuness <jfuruness@gmail.com>
2022-03-31 02:20:58 -0400

    v2a check

--- lib_rovpp/__main__.py
+++ lib_rovpp/__main__.py
@@ -21,51 +21,28 @@ from .engine_input import ROVPPSubprefixHijack
 from .engine_input import ROVPPNonRoutedSuperprefixHijack
 from .engine_input import ROVPPSuperprefixPrefixHijack
 from .engine_input import ROVPPPrefixHijack
+from .engine_input import ROVPPNonRoutedPrefixHijack
 
-
-default_kwargs = {"percent_adoptions": [0, 5, 10, 20, 30, 40, 60, 80, 100],
-                  "num_trials": 100}
-
-non_lite_policies =(ROVPPV1SimpleAS,
-                    ROVPPV2SimpleAS,
-                    ROVPPV2aSimpleAS,
-                    ROVPPV2ShortenSimpleAS,
-                    )
-
-rov_non_lite_rovpp = (ROVAS,) + non_lite_policies
+default_kwargs = {"percent_adoptions": [0, 20, 60],
+                  "num_trials": 1}
 
 
 def run_sim(graph, path):
-    sim = Simulator(parse_cpus=11)
+    sim = Simulator(parse_cpus=8)
 
-    sim.run(graphs=[graph], graph_path=path, mp_method=MPMethod.MP)
+    sim.run(graphs=[graph], graph_path=path, mp_method=MPMethod.SINGLE_PROCESS)
 
 
 def main():
-
     assert isinstance(input("Turn asserts off for speed?"), str)
 
     # Get's all of the general graphs for all types of attacks
-    for atk in [ROVPPSubprefixHijack,
-                ROVPPNonRoutedSuperprefixHijack,
-                ROVPPSuperprefixPrefixHijack,
-                ROVPPPrefixHijack]:
-        if atk == ROVPPSubprefixHijack:
-            pols = rov_non_lite_rovpp + (ROVPPV3AS,)
-        else:
-            pols = rov_non_lite_rovpp
-        kwargs = {"EngineInputCls": atk, **default_kwargs}
-        graph = Graph(adopt_as_classes=pols, **kwargs)
-
-        run_sim(graph, Path(f"/home/anon/{atk.__name__}_graphs.tar.gz"))
-        print(f"Completed {atk}")
-        print("Ending early")
-        return
-
-    kwargs = {"EngineInputCls": ROVPPSubprefixHijack, **default_kwargs}
-    graph = Graph(adopt_as_classes=[ROVPPV1LiteSimpleAS, ROVPPV1SimpleAS],
-                  **kwargs)
-    run_sim(graph, Path("/home/anon/v1_lite_vs_non_lite_graphs.tar.gz"))
+    atk = ROVPPNonRoutedPrefixHijack
+    pols = (ROVPPV2aSimpleAS, ROVPPV2SimpleAS)
+    kwargs = {"EngineInputCls": atk, **default_kwargs}
+    graph = Graph(adopt_as_classes=pols, **kwargs)
+    run_sim(graph, Path(f"/home/anon/v2a_check_graphs.tar.gz"))
+    print(f"Completed {atk}")
 
 if __name__ == "__main__":
     start = datetime.now()
